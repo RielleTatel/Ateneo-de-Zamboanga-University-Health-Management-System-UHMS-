e0971bb729577db6c8bb378b78264074cdc16cfe
RielleTatel <tatelgabrielle19@gmail.com>
2025-09-23 13:38:23 +0800

    env set-up

---
+++ client/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <div className="bg-red-500 w-64 h-32 rounded-lg shadow-lg flex items-center justify-center"> 
+        <h1 className="text-white text-2xl font-bold"> 
+          Hello Tailwind!
+        </h1>
+      </div>
+    </div>
+  )
+}
+
+export default App
